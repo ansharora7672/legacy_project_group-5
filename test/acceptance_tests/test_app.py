73df2a45a36728a1c2309becba68e197cf0b2407
AnujMongia1 <amongia1308@conestogac.on.ca>
2024-11-20 21:07:44 -0500

    update to trigger test workflow 5

--- test/acceptance_tests/test_app.py
+++ test/acceptance_tests/test_app.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import shutil
 import pytest
 from PIL import Image
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from app import app
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
